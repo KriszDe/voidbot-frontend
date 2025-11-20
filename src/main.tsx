bda81fe23c9242538699072e0dcd4ee6a5e7cf54
KriszDe <kovacskrisztofer56@gmail.com>
2025-11-21 00:43:32 +0100

    dw2

--- src/main.tsx
+++ src/main.tsx
@@ -1,26 +1,19 @@
-// src/main.tsx
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './index.css'
+import App from './App'              // ← landing (fő oldal)
+import Home from './pages/Home'      // ← bejelentkezett főoldal
+import AuthCallback from './pages/AuthCallback'
 
-import App from "./App";              // landing (fő oldal, voidbot.hu/)
-import Home from "./pages/Home";      // bejelentkezett oldal (voidbot.hu/home)
-import AuthCallback from "./pages/AuthCallback"; // voidbot.hu/auth/callback
-
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        {/* Fő marketing / landing oldal */}
-        <Route path="/" element={<App />} />
-
-        {/* Discord OAuth callback */}
+        <Route path="/" element={<App />} /> {/* fő oldal */}
+        <Route path="/home" element={<Home />} />       {/* bejelentkezett oldal */}
         <Route path="/auth/callback" element={<AuthCallback />} />
-
-        {/* Belépés utáni app */}
-        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
-  </StrictMode>
-);
+  </StrictMode>,
+)
