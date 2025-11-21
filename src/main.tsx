fdec6607914b793b3a5a626bd9bb664d69e995df
KriszDe <kovacskrisztofer56@gmail.com>
2025-11-21 10:55:36 +0100

    m

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import './index.css'
-import App from './App'              // ← landing (fő oldal)
-import Home from './pages/Home'      // ← bejelentkezett főoldal
+
+import App from './App'
+import Home from './pages/Home'
 import AuthCallback from './pages/AuthCallback'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} /> {/* fő oldal */}
-        <Route path="/home" element={<Home />} />       {/* bejelentkezett oldal */}
-        <Route path="/auth/callback" element={<AuthCallback />} />
+
+        {/* Főoldal */}
+        <Route path="/" element={<App />} />
+
       </Routes>
     </BrowserRouter>
   </StrictMode>,
