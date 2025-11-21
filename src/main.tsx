7d75930ca04699bcee82473b3683f2d67213b710
KriszDe <kovacskrisztofer56@gmail.com>
2025-11-21 10:56:55 +0100

    m

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 
 import App from './App'
-import Home from './pages/Home'
-import AuthCallback from './pages/AuthCallback'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
