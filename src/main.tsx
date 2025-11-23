769d7e849a69af7f0ff5922a71edc1712da11e8a
KriszDe <kovacskrisztofer56@gmail.com>
2025-11-23 10:47:19 +0100

    lading page

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import './index.css'
+import './App.css'
 
 import App from './App'
 import Home from './pages/Home'
