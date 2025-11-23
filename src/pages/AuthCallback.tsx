8ea8e7bef2ddcf62ac39c27c36bd2660fa6f9dca
KriszDe <kovacskrisztofer56@gmail.com>
2025-11-23 10:26:57 +0100

    oo

--- src/pages/AuthCallback.tsx
+++ src/pages/AuthCallback.tsx
@@ -24,16 +24,13 @@ export default function AuthCallback() {
 
         const API_BASE = import.meta.env.VITE_API_URL;
 
-        // Backend hívása
-        const res = await fetch(`${API_BASE}/api/auth/discord`, {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({
-            code,
-            redirect_uri: window.location.origin + "/auth/callback"
-          })
-        });
+      const res = await fetch(`${API_BASE}/api/auth/discord`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ code })
+    });
 
+      
         const data = await res.json();
 
         if (!res.ok || !data.user) {
